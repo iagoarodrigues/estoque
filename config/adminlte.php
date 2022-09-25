40c959bbe06573991fddc302eda007a7324a1377
Iago Rodrigues <102769741+iagoarodrigues@users.noreply.github.com>
2022-09-25 17:52:00 -0300

    atualização do projeto

--- config/adminlte.php
+++ config/adminlte.php
@@ -63,9 +63,9 @@ return [
     |
     */
 
-    'logo' => '<b>Admin</b>LTE',
-    'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
-    'logo_img_class' => 'brand-image img-circle elevation-3',
+    'logo' => '<b>FORÚM DA BELEZA</b>',
+    'logo_img' => 'vendor/adminlte/dist/img/fdb.png',
+    'logo_img_class' => 'brand-image img-rounded',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
     'logo_img_alt' => 'Admin Logo',
